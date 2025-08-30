9cb243fb56054d47b9f2690b049f39b86a8feceb
armanabramov <128611537+armanabramov@users.noreply.github.com>
2025-08-30 22:01:45 +0300

    feat: add getRandomCocktail fn in cocktail module

--- src/modules/cocktail.module.js
+++ src/modules/cocktail.module.js
@@ -6,7 +6,22 @@ export class Cocktail extends Module {
 		this.apiUrl = 'www.thecocktaildb.com/api/json/v1/1/random.php';
 	}
 	trigger() {
-		
+
 	}
 
+	async getRandomCocktail() {
+		try {
+			const res = await fetch(apiUrl);
+			if(!res.ok) {
+				throw new Error (`Ошибка при получении коктейля. Статус ${res.status}`)
+			}
+			console.log(res)
+			const data = await res.json();
+			console.log(data)
+
+		} catch (error) {
+			console.error('Ошибка при получении коктейля:', error);
+			return
+		}
+	}
 }
